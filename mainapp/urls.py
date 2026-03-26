d73ee3064097fecfb00b34128b0d97a17bec4d7f
miramir <xaydarovmiramir@gmail.com>
2026-03-26 07:59:49 +0500

    First commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,3 +1,8 @@
-from .views import home
+from .views import home,fakultet,kafedra,fanlar,ustozlar,talabalar
 from django.urls import path
-urlpatterns=[path('',home,name='home'),]
+urlpatterns=[path('',home,name='home'),
+             path('fakultet/',fakultet,name='fakultet'),
+             path('kafedra/',kafedra,name='kafedra'),
+             path('fanlar/',fanlar,name='fanlar'),
+             path('ustozlar/',ustozlar,name='ustozlar'),
+             path('talabalar/',talabalar,name='talabalar')]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+def fakultet(request):
+    return render(request,'fakultet.html')
+def kafedra(request):
+    return render(request,'kafedra.html')
+def fanlar(request):
+    return render(request,'fanlar.html')
+def ustozlar(request):
+    return render(request,'ustozlar.html')
+def talabalar(request):
+    return render(request,'talabalar.html')
